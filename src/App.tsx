eaabd1955b25d0422886bc684f26a1ef29d57489
Kseniia Pidopryhora <ksenia.pidopryhora@gmail.com>
2023-05-02 10:10:54 +0300

    add BannerComponent

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import React from "react";
-// import Banner from "./components/Banner/Banner";
+import Banner from "./components/Banner/Banner";
 
 function App() {
   return (
     <div className="app">
-      {/* <Banner /> */}
-      {/* <Banner /> */}
+      <Banner />
 
     </div>
   );
