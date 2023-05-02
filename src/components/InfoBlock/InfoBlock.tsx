99c13fe7443a9b5c9b0a2cafee50840ce3829dda
Alyona Chernenko <errorroror75@gmail.com>
2023-05-02 09:40:51 +0300

    change filenames

--- src/components/InfoBlock/InfoBlock.tsx
+++ src/components/InfoBlock/InfoBlock.tsx
@@ -1,4 +1,4 @@
-import styles from "./index.module.scss";
+import styles from "./InfoBlock.module.scss";
 
 import { FaTelegramPlane } from "react-icons/fa";
 import { IoLogoWhatsapp } from "react-icons/io";
