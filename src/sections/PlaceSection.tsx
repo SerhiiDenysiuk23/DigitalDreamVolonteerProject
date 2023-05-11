d5016f4dd10258b04656b5a44b2270e7a351504b
Сергій Денисюк <kn202_dsm@student.ztu.edu.ua>
2023-05-12 02:59:35 +0300

    Adapted to tablet PlaceSection

--- src/sections/PlaceSection.tsx
+++ src/sections/PlaceSection.tsx
@@ -1,10 +1,11 @@
 import React, {CSSProperties} from 'react';
 import PlacesPhotoSlider from "../components/Places/PlacesPhotoSlider";
 import PlaceInfo from "../components/Places/PlaceInfo";
+import styles from "../components/Places/placesSection.module.scss"
 
 const PlaceSection = () => {
     return (
-        <section>
+        <section className={styles.placeSection}>
             <PlacesPhotoSlider/>
             <PlaceInfo/>
         </section>
