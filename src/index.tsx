87e3fde2fdb972817b033a2a5b755cfcc18f3cf5
Alyona Chernenko <errorroror75@gmail.com>
2023-05-12 14:28:48 +0300

    update

--- src/index.tsx
+++ src/index.tsx
@@ -13,28 +13,9 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
-<<<<<<< HEAD
-const client = new ApolloClient({
-  uri: 'https://api.uadna.art/graphql',
-  cache: new InMemoryCache(),
-  defaultOptions: {
-    watchQuery: {
-      fetchPolicy: 'cache-and-network',
-    },
-  },
-  resolvers: {},
-});
 
-root.render(
-  <ApolloProvider client={client}>
-  <Provider store={store}>
-    <App />
-  </Provider>
-  </ApolloProvider>
-=======
 root.render(
     <ApolloProvider client={client}>
         <App />
     </ApolloProvider>
->>>>>>> 4bd5764b982f1cb07a37b040af697d1fbbe53208
 );
