ed87c882bb4b54f3e51f5d1ab50a63372099a103
Alyona Chernenko <errorroror75@gmail.com>
2023-05-17 18:39:36 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import Banner from "./sections/Banner/Banner";
 import DonateSection from "./sections/DonateSection/DonateSection";
 import DonateSectionSecond from "./sections/DonateSectionSecond/DonateSectionSecond";
 import MiddleSlider from "./components/MiddleSlider/MiddleSlider";
-import { MusicSection } from "./sections/MusicSection";
+import { MusicSection } from "./sections/MusicSection/MusicSection";
 import PlaceSection from "./sections/PlaceSection";
 import SolutionsSection from "./sections/SolutionsSection";
 
