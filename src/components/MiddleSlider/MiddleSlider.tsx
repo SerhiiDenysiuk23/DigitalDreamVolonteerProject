8c84847053e840cbf18cb037bde1e158f5829536
Dima-Bradarskiy <bradarskiy.dmitriy@gmail.com>
2023-05-19 13:30:17 +0300

    Complete adaptive and pagination  for  middle slider

--- src/components/MiddleSlider/MiddleSlider.tsx
+++ src/components/MiddleSlider/MiddleSlider.tsx
@@ -1,91 +1,76 @@
 import SliderItem from "./SliderItem/SliderItem"
 import style from './MiddleSlider.module.scss'
-import {useState, useEffect, useRef, useCallback, RefObject} from "react"
-import Slider, {InnerSlider, Settings} from 'react-slick'
+import { useState, useEffect, useRef, useCallback, RefObject } from "react"
+import Slider, { InnerSlider, Settings } from 'react-slick'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import Paginator from "./Paginator/Paginator";
-
-
 
 
 let sliderItems = {
     arts: [
-        {img: '/assets/MiddleSliderImages/arts/forSlider.png', description: "Society that"},
-        {img: '/assets/MiddleSliderImages/arts/img-2.png', description: "Society that"},
-        {img: '/assets/MiddleSliderImages/arts/img-3.png', description: "Society that"},
-        {img: '/assets/MiddleSliderImages/arts/img-4.png', description: "Society that"},
-        {img: '/assets/MiddleSliderImages/arts/img-2.png', description: "Society that"},
-        {img: '/assets/MiddleSliderImages/arts/forSlider.png', description: "Society that"},
+        { img: 'https://picsum.photos/300', description: "Society that" },
+        { img: 'https://picsum.photos/200', description: "Society that" },
+        { img: 'https://picsum.photos/400', description: "Society that" },
+        { img: 'https://picsum.photos/500', description: "Society that" },
+        { img: 'https://picsum.photos/100', description: "Society that" },
+        { img: 'https://picsum.photos/600', description: "Society that" },
+        // {img: '/assets/MiddleSliderImages/arts/img-2.png', description: "Society that"},
+        // {img: '/assets/MiddleSliderImages/arts/img-3.png', description: "Society that"},
+        // {img: '/assets/MiddleSliderImages/arts/img-4.png', description: "Society that"},
+        // {img: '/assets/MiddleSliderImages/arts/img-2.png', description: "Society that"},
+        // {img: '/assets/MiddleSliderImages/arts/forSlider.png', description: "Society that"},
 
-        {img: '/assets/MiddleSliderImages/arts/forSlider.png', description: "Society that"},
-        {img: '/assets/MiddleSliderImages/arts/img-2.png', description: "Society that"},
-        {img: '/assets/MiddleSliderImages/arts/img-3.png', description: "Society that"},
-        {img: '/assets/MiddleSliderImages/arts/img-4.png', description: "Society that"},
-        {img: '/assets/MiddleSliderImages/arts/img-2.png', description: "Society that"},
-        {img: '/assets/MiddleSliderImages/arts/forSlider.png', description: "Society that"},
+        { img: '/assets/MiddleSliderImages/arts/forSlider.png', description: "Society that" },
+        { img: '/assets/MiddleSliderImages/arts/img-2.png', description: "Society that" },
+        { img: '/assets/MiddleSliderImages/arts/img-3.png', description: "Society that" },
+        { img: '/assets/MiddleSliderImages/arts/img-4.png', description: "Society that" },
+        { img: '/assets/MiddleSliderImages/arts/img-2.png', description: "Society that" },
+        { img: '/assets/MiddleSliderImages/arts/forSlider.png', description: "Society that" },
 
-        {img: '/assets/MiddleSliderImages/arts/forSlider.png', description: "Society that"},
-        {img: '/assets/MiddleSliderImages/arts/img-2.png', description: "Society that"},
-        {img: '/assets/MiddleSliderImages/arts/img-3.png', description: "Society that"},
-        {img: '/assets/MiddleSliderImages/arts/img-4.png', description: "Society that"},
-        {img: '/assets/MiddleSliderImages/arts/img-2.png', description: "Society that"},
-        {img: '/assets/MiddleSliderImages/arts/forSlider.png', description: "Society that"}
+        { img: 'https://picsum.photos/800', description: "Society that" },
+        { img: 'https://picsum.photos/900', description: "Society that" },
+        { img: 'https://picsum.photos/1000', description: "Society that" },
+        { img: 'https://picsum.photos/1100', description: "Society that" },
+        { img: 'https://picsum.photos/1200', description: "Society that" },
+        { img: 'https://picsum.photos/1300', description: "Society that" },
+        { img: 'https://picsum.photos/1400', description: "Society that" },
+
+        // {img: '/assets/MiddleSliderImages/arts/forSlider.png', description: "Society that"},
+        // {img: '/assets/MiddleSliderImages/arts/img-2.png', description: "Society that"},
+        // {img: '/assets/MiddleSliderImages/arts/img-3.png', description: "Society that"},
+        // {img: '/assets/MiddleSliderImages/arts/img-4.png', description: "Society that"},
+        // {img: '/assets/MiddleSliderImages/arts/img-2.png', description: "Society that"},
+        // {img: '/assets/MiddleSliderImages/arts/forSlider.png', description: "Society that"}
     ]
 }
 
-
-interface CustomInnerSlider extends InnerSlider {
-    props: {
-
-        children: [];
-    };
-    state: {
-        currentSlide: number;
-        slideCount: number;
-    };
-}
-
 const MiddleSlider = () => {
-    // Состояние для текущего слайда и количества слайдов
-    const [currentSlide, setCurrentSlide] = useState(0);
-    const [slideCount, setSlideCount] = useState(0);
+    function SampleNextArrow(props: any) {
+        const { onClick } = props;
+        return (
 
-    const sliderRef = useRef<Slider>(null)
+            <img src="/assets/MiddleSliderImages/arts/Arrow.png" alt="" onClick={onClick} className={`${style.prevnext}, ${style.next}`} />
+        );
+    }
 
-    // Функция для обновления состояния при изменении слайда
-    const updateState = (current: number, count: number) => {
-        setCurrentSlide(current);
-        setSlideCount(count);
-    };
+    function SamplePrevtArrow(props: any) {
+        const { onClick } = props;
+        return (
 
-
-    const goToNextSlide = () => {
-        sliderRef.current?.slickNext();
-    };
-
-    const goToPrevSlide = () => {
-        sliderRef.current?.slickPrev();
-    };
-
-    const goToSlide = (index: number) => {
-        sliderRef.current?.slickGoTo(index);
-    };
-
-
+            <img src="/assets/MiddleSliderImages/arts/Arrow.png" alt="" onClick={onClick} className={`${style.prevnext}, ${style.prev}`} />
+        );
+    }
 
     const settings: Settings = {
-        dots: false, // Отключаем стандартные точки навигации
-        arrows: false, // Отключаем стандартные стрелки навигации
-        infinite: false, 
+        dots: true,
+        arrows: true, 
+        infinite: true,
         speed: 500,
         slidesToShow: 3,
         slidesToScroll: 1,
         rows: 2,
-        beforeChange: (current, next) => updateState(next, slideCount), // Обновляем состояние перед изменением слайда
-        afterChange: (current) => updateState(current, slideCount), // Обновляем состояние после изменения слайда
-        onReInit: () => updateState(currentSlide, (sliderRef.current?.innerSlider as CustomInnerSlider)?.state.slideCount), // Обновляем состояние при повторной инициализации слайдера
-        onInit: () => updateState(currentSlide, (sliderRef.current?.innerSlider as CustomInnerSlider)?.state.slideCount), // Обновляем состояние при первой инициализации слайдера
+        nextArrow: <SampleNextArrow />,
+        prevArrow: <SamplePrevtArrow />,
         responsive: [
             {
                 breakpoint: 1024,
@@ -96,25 +81,30 @@ const MiddleSlider = () => {
                     slidesToScroll: 1,
                 }
             },
+            {
+                breakpoint: 500,
+                settings: {
+                    speed: 500,
+                    slidesToShow: 2,
+                    rows: 1,
+                    slidesToScroll: 1,
+                }
+            },
         ]
     };
 
 
-   
+
     return (
         <div className={`${style.container} middle-slider-container`}>
             <div className={style.heading}>
                 <h3 className={style.header}>Do you know about Ukrainian ART?</h3>
-                <div>
-                    <Paginator goTo={goToSlide} prev={goToPrevSlide} next={goToNextSlide} currentSlide={currentSlide} slideCount={slideCount}/>
-                </div>
             </div>
             <div className={style.slide}>
-                <Slider ref={sliderRef} className={`${style.slider}  `} {...settings}>
+                <Slider className={`${style.slider} middle-slider `} {...settings}>
                     {
-
                         sliderItems.arts.map(item => (
-                            <SliderItem key={item.img} image={item.img} description={item.description}/>
+                            <SliderItem key={item.img} image={item.img} description={item.description} />
                         ))
                     }
 
