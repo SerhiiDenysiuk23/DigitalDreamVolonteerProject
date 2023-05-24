a74262b747ff43e66640e1cd65e91fed660b02a8
Сергій Денисюк <kn202_dsm@student.ztu.edu.ua>
2023-05-24 03:20:46 +0300

    Added popup to Brands and Solutions

--- src/components/Brands/BrandsSlider.tsx
+++ src/components/Brands/BrandsSlider.tsx
@@ -1,11 +1,17 @@
-import React, {FC} from 'react';
+import React, {FC, useState} from 'react';
 import Slider, {Settings} from "react-slick";
 import styles from "./styles/BrandsSection.module.scss";
 import ArrowSliderBtn from "../../elements/ArrowSliderBtn/ArrowSliderBtn";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import Popup from "../../elements/Popup/Popup";
 
 const BrandsSlider: FC<{achievement: string}> = ({achievement})=> {
+    const [showModal, setShowModal] = useState<boolean>(false);
+    const [currentSlide, setCurrentSlide] = useState(0)
+    const handleModal = () => {
+        setShowModal(!showModal)
+    };
     const photos = [
         'https://picsum.photos/250',
         'https://picsum.photos/251',
@@ -25,6 +31,9 @@ const BrandsSlider: FC<{achievement: string}> = ({achievement})=> {
         customPaging(index: number): JSX.Element {
             return <img src={photos[index]}/>
         },
+        afterChange(currentSlide: number) {
+            setCurrentSlide(currentSlide)
+        },
         responsive: [
             {
                 breakpoint: 995,
@@ -35,10 +44,12 @@ const BrandsSlider: FC<{achievement: string}> = ({achievement})=> {
         ]
     };
 
+    const data = { name: "", description: "", link: photos[currentSlide] }
     const title = "TM byMe"
     const slogan = "З А К О Х У В А Т И"
     return (
         <div className={styles.sliderSide}>
+            {showModal && <Popup data={data} handleModal={handleModal} />}
             <h2>{title}</h2>
             <div className={styles.slogan}>{slogan}</div>
             <Slider className={styles.brandSlider} {...settings}>
